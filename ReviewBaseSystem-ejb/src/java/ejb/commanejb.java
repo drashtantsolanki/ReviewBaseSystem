531a76874e3865eecd35c30311dcff2b1d64acb9
moheetjari <moheetjari@outlook.com>
2020-02-27 15:02:51 +0530

    shreya first commit

--- ReviewBaseSystem-ejb/src/java/ejb/commanejb.java
+++ ReviewBaseSystem-ejb/src/java/ejb/commanejb.java
@@ -5,7 +5,14 @@
  */
 package ejb;
 
+import entity.Author;
+import entity.Company;
+import entity.Genre;
+import entity.Publisher;
+import java.util.Collection;
 import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 
 /**
  *
@@ -14,6 +21,148 @@ import javax.ejb.Stateless;
 @Stateless
 public class commanejb implements commanejbLocal {
 
+    @PersistenceContext(unitName = "MyReview")
+    EntityManager em;
+//*********Author Information************//
+
+    @Override
+    public void addAuthor(String AuthorName) {
+
+        Author a = new Author();
+        a.setAuthorName(AuthorName);
+        em.persist(a);
+    }
+
+    @Override
+    public void updateAuthor(int AuthorId, String AuthorName) {
+
+        Author auth = em.find(Author.class, AuthorId);
+        auth.setAuthorName(AuthorName);
+        em.merge(auth);
+
+        //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Author getAuthorByName(String AuthorName) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Collection<Author> getAllAuthor() {
+        return em.createNamedQuery("Author.findAll").getResultList();//To change body of generated methods, choose Tools | Templates.
+    }
+
     // Add business logic below. (Right-click in editor and choose
     // "Insert Code > Add Business Method")
+    @Override
+    public void removeAuthor(int AuthorId, String AuthorName) {
+        Author a = em.find(Author.class, AuthorId);
+        em.remove(a);
+        //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Author getAuthorById(int AuthorId) {
+        //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        Author objauthor = (Author) em.find(Author.class, AuthorId);
+        return objauthor;
+    }
+    //*********Company Information************//
+
+    /*public void addCompny(String CompanyName) {
+        
+       Company c = new Company();
+       c.setCompanyName(CompanyName);
+       em.persist(c);
+        }*/
+    @Override
+    public void addCompany(String CompanyName) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void updateComapany(int CompanyId, String CompanyName) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void removeCompany(int CompanyId, String CompanyName) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Company getCompanuById(int CompanyId) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Company getCompanyByName(String CompanyName) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Collection<Company> getAllCompany() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void addGenre(String GenreName) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void updateGenre(int GenreId, String GenreName) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void removeGenre(int GenreId, String GenreName) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Genre getGenreById(int GenreId) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Genre getGenreByName(String GenreName) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Collection<Genre> getAllGenre() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void addPublisher(String PublisherName) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void updatePublisher(int PublisherId, String PublisherName) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void removePublisher(int PublisherId, String PublisherName) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Publisher getPublisherById(int PublisherId) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Publisher getPublisherByName(String PublisherName) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Collection<Publisher> getAllPublisher() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
 }
--- ReviewBaseSystem-ejb/src/java/ejb/commanejbLocal.java
+++ ReviewBaseSystem-ejb/src/java/ejb/commanejbLocal.java
@@ -5,6 +5,11 @@
  */
 package ejb;
 
+import entity.Author;
+import entity.Company;
+import entity.Genre;
+import entity.Publisher;
+import java.util.Collection;
 import javax.ejb.Local;
 
 /**
@@ -13,5 +18,56 @@ import javax.ejb.Local;
  */
 @Local
 public interface commanejbLocal {
-    
+    //************Author Methods***************//
+
+    void addAuthor(String AuthorName);
+
+    void updateAuthor(int AuthorId, String AuthorName);
+
+    void removeAuthor(int AuthorId, String AuthorName);
+
+    Author getAuthorById(int AuthorId);
+
+    Author getAuthorByName(String AuthorName);
+
+    Collection<Author> getAllAuthor();
+
+    //************Company Methods***************//
+    void addCompany(String CompanyName);
+
+    void updateComapany(int CompanyId, String CompanyName);
+
+    void removeCompany(int CompanyId, String CompanyName);
+
+    Company getCompanuById(int CompanyId);
+
+    Company getCompanyByName(String CompanyName);
+
+    Collection<Company> getAllCompany();
+
+    //*********Genre Methods************//
+    void addGenre(String GenreName);
+
+    void updateGenre(int GenreId, String GenreName);
+
+    void removeGenre(int GenreId, String GenreName);
+
+    Genre getGenreById(int GenreId);
+
+    Genre getGenreByName(String GenreName);
+
+    Collection<Genre> getAllGenre();
+
+    //************Publisher Methods***************//
+    void addPublisher(String PublisherName);
+
+    void updatePublisher(int PublisherId, String PublisherName);
+
+    void removePublisher(int PublisherId, String PublisherName);
+
+    Publisher getPublisherById(int PublisherId);
+
+    Publisher getPublisherByName(String PublisherName);
+
+    Collection<Publisher> getAllPublisher();
 }
